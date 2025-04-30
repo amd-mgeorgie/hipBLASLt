ca49a1e9969489de5d73459ef4e726c8b23274f5
Mihail <mgeorgie@banff-cyxtera-s77-1.ctr.dcgpu>
2025-04-30 08:46:52 +0000

    Keeping the code neat

--- library/src/amd_detail/rocblaslt/src/UserDrivenTuningParser.cpp
+++ library/src/amd_detail/rocblaslt/src/UserDrivenTuningParser.cpp
@@ -147,7 +147,7 @@ namespace TensileLite
             outputType   = hipDataType_to_tensile_type(string_to_hip_datatype(entries[19]));
             computeType  = hipDataType_to_tensile_type(string_to_hip_datatype(entries[21]));
             // computeType  = hipDataType_to_tensile_compute_type(string_to_hipblas_computetype(entries[21]));
-            // TODO: Workaround for TF32 support, should be handled with the above,
+            // TODO: Workaround for TF32 support, should be handled with the above
             
             if (entries[21] == "xf32_r")            
             {
